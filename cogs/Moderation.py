15c877df72899daad358043a7d307a3f9594d32f
0xcabrex <asvamogh@gmail.com>
2020-11-02 00:28:50 +0530

    major reconstruction and added aesthetics

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1365,7 +1365,10 @@ class ModerationCog(commands.Cog):
 
                     delete = delete_warns(multiple_member_array[0].guild.id, multiple_member_array[0].id)
 
-                    await ctx.send(f"{ctx.author.mention}, cleared **{number_of_infractions}** warnings for **{multiple_member_array[0].name}**")
+                    if number_of_infractions == 1:
+                        await ctx.send(f"{ctx.author.mention}, cleared **{number_of_infractions}** warning for **{multiple_member_array[0].name}**")
+                    else:
+                        await ctx.send(f"{ctx.author.mention}, cleared **{number_of_infractions}** warning for **{multiple_member_array[0].name}**")
 
                 else:
                     await ctx.send(f"**{multiple_member_array[0].name}** does not have any infractions to clear")
